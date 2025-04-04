42ccec7058e4d7e99e5aab47e04af7648f4e1753
Peter Danulf <73417013+peterdanulf@users.noreply.github.com>
2025-04-04 17:03:38 +0200

    make format rule specific for php

--- nvim/lua/plugins/format-php.lua
+++ nvim/lua/plugins/format-php.lua
@@ -11,11 +11,18 @@ return {
       -- Set up PHP formatting with prettier
       opts.formatters_by_ft = opts.formatters_by_ft or {}
       opts.formatters_by_ft.php = { "prettier" }
-
-      -- Add printWidth setting for 80 characters
+      
+      -- Add printWidth setting for 80 characters only for PHP
       opts.formatters = opts.formatters or {}
       opts.formatters.prettier = {
-        prepend_args = { "--print-width", "80" },
+        prepend_args = function(self, ctx)
+          -- Only apply 80 character limit to PHP files
+          if ctx.filename:match("%.php$") then
+            return { "--print-width", "80" }
+          end
+          -- For other file types, use default settings
+          return {}
+        end,
       }
     end,
   },
