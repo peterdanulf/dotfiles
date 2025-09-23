b4bb335ad57c1140b809db6ca5eccf13ec13d598
Peter Danulf <73417013+peterdanulf@users.noreply.github.com>
2025-09-23 18:01:05 +0200

    fix treesitter

--- nvim/lua/plugins/treesitter-php.lua
+++ nvim/lua/plugins/treesitter-php.lua
@@ -1,27 +1,11 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
-    lazy = false,
-    opts = {
-      ensure_installed = {
+    opts = function(_, opts)
+      -- Ensure PHP is in the list of installed parsers
+      vim.list_extend(opts.ensure_installed, {
         "php",
-      },
-      highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = false,
-      },
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = "<C-Space>", -- starta
-          node_incremental = "<C-Space>", -- expandera
-          node_decremental = "<BS>", -- krymp
-          scope_incremental = false,
-        },
-      },
-    },
-    config = function(_, opts)
-      require("nvim-treesitter.config").setup(opts)
+      })
     end,
   },
 }
