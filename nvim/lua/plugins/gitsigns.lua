1955e2aca0c2da03989c16c1641ceeeeb07a7511
Peter Danulf <peter.danulf@gmail.com>
2026-01-21 15:45:52 +0100

    fix previous fix

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -1,10 +1,7 @@
 return {
   "lewis6991/gitsigns.nvim",
-  opts = function(_, opts)
-    opts.on_attach = function(buf)
-      local gs = package.loaded.gitsigns
-      vim.keymap.set("n", "ö", gs.next_hunk, { buffer = buf })
-      vim.keymap.set("n", "ä", gs.prev_hunk, { buffer = buf })
-    end
-  end,
+  keys = {
+    { "ö", function() require("gitsigns").next_hunk() end, desc = "Next Hunk" },
+    { "ä", function() require("gitsigns").prev_hunk() end, desc = "Prev Hunk" },
+  },
 }
